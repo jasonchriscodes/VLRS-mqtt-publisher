8ca0d1bf5005064546c58b4f8a87c200d7b01f83
Jason Christian <jasonchristian1234@gmail.com>
2024-07-02 22:27:17 +1200

    chore: customize mode dialog box

--- app/src/main/java/com/jason/publisher/SplashScreen.kt
+++ app/src/main/java/com/jason/publisher/SplashScreen.kt
@@ -2,6 +2,7 @@ package com.jason.publisher
 
 import android.Manifest
 import android.annotation.SuppressLint
+import android.app.Dialog
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.location.Location
@@ -12,6 +13,7 @@ import android.os.Handler
 import android.os.Looper
 import android.provider.Settings
 import android.util.Log
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
@@ -86,24 +88,25 @@ class SplashScreen : AppCompatActivity() {
      * Shows the mode selection dialog and handles the selected mode.
      */
     private fun showOptionDialog() {
-        modeSelectionDialog.showModeSelectionDialog(object : ModeSelectionDialog.ModeSelectionListener {
-            override fun onModeSelected(mode: String) {
-                var intent = Intent(this@SplashScreen, MainActivity::class.java)
-                if (mode == "Offline") {
-                    intent = Intent(this@SplashScreen, OfflineActivity::class.java)
-                }
-                intent.putExtra(Constant.deviceNameKey, name)
-                intent.putExtra(Constant.tokenKey, accessToken)
-                intent.putExtra(Constant.aidKey, aaid)
-                intent.putExtra("lat", latitude)
-                intent.putExtra("lng", longitude)
-                intent.putExtra("ber", bearing)
-                intent.putExtra("spe", speed)
-                intent.putExtra("dir", direction)
-                startActivity(intent)
-                finish()
-            }
-        })
+        val dialog = Dialog(this)
+        dialog.setContentView(R.layout.mode_selection_dialog)
+
+        val onlineModeButton = dialog.findViewById<Button>(R.id.onlineModeButton)
+        val offlineModeButton = dialog.findViewById<Button>(R.id.offlineModeButton)
+
+        onlineModeButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            dialog.dismiss()
+        }
+
+        offlineModeButton.setOnClickListener {
+            val intent = Intent(this, OfflineActivity::class.java)
+            startActivity(intent)
+            dialog.dismiss()
+        }
+
+        dialog.show()
     }
 
     /**
